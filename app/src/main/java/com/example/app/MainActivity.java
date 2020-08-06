3d191539db36ca1c5ee7c9e63dac8960c5b6c865
Raviteja Guttula <thisisgvrt@gmail.com>
2020-08-05 23:52:13 -0700

    updated for gator-connect

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         mWebView.setWebViewClient(new MyWebViewClient());
 
         // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+        mWebView.loadUrl("https://gator-connect.netlify.app/");
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
